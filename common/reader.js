06239827d620862c1838d8befe9b71d0e3303e58
Boris <6479500dev@gmail.com>
2024-12-01 20:15:18 +0400

    01 common and day one

---
+++ common/reader.js
@@ -0,0 +1,9 @@
+import { fileURLToPath } from "url";
+import fs from "fs";
+import path from "path";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const getPath = (fileName) => path.join(dirname, fileName);
+export default (fileName) => fs.readFileSync(getPath(fileName), "utf-8");
